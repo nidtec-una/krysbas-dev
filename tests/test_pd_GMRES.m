ac526e0ec1cfa481b64c9b4303486e84d6cb0e42
Jhabriel Varela <jhabriel@gmail.com>
2023-08-28 21:54:10 +0200

    TST: First try at making a real test

--- tests/test_pd_GMRES.m
+++ tests/test_pd_GMRES.m
@@ -12,93 +12,35 @@ function test_suite = test_pd_GMRES %#ok<*STOUT>
 
 end
 
-function test_pd_GMRES_dummy()
+function test_pd_GMRES_0()
 
-    assertEqual(10, 8 + 2)
+    sol7=[]; %PD-GMRES(m)
+
+    % Load A and b from the sherman3 matrix
+    rootFolder = fileparts(pwd); % go to the root folder
+    dataFolder = fullfile(rootFolder, 'data'); % enter the data folder
+    load 'sherman3.mat' Problem  %
+
+    % Matrix A and right-hand-side b
+    A=Problem.A;
+    b=Problem.b;
+
+    % Parameters
+    alpha=-3; %2
+    delta=5; %0.8
+    opts_tol=1e-9;
+    itermax=1000;
+    p = 1;
+
+    %%       %PD-GMRES(m)
+    % The original idea was to compute the average execution time,
+    % we may discuss if this is still necessary 
+    for i=1:p
+        color_pd_gmres='b';
+        mPD=30;
+        %alpha=2;
+        [time, logres_pd_gmres]=pd_GMRES(A,b, mPD, alpha, delta,itermax);
+        sol7(size(sol7,1)+1,:)= [time];
+    end
 
 end
-
-% load 'matrices\sherman5.mat';
-% A=Problem.A;
-% b=Problem.b;
-% OR
-% b=b(:,1);
-% OR
-% b=ones(size(A,1),1);
-
-%Calculo de coeficientes de polinomio de GMRES(m)
-%         n1 = m;
-%         C = [];
-%         for i = 1:n1
-%             if i==1
-%                 C(i, i) = 1/beta;
-%             end
-%             
-%             B1 = [0; C(:, i)];
-%             hs = [];
-%             for j = 1:i
-%                 hs = Hs(1:j, j);
-%             end
-%             B2 = [C *  hs; 0];
-%             C(i + 1, i + 1) = 0;
-%             C(:, i + 1) = inv(Hs(i + 1, i)) *  B1 - inv(Hs(i + 1, i)) *  B2;
-%         end
-% %Calculos de cooeficientes
-%         U = C;
-%         C(:, m + 1) = [];
-%         C(m + 1, :) = [];
-%         coef = C *  minimizer;
-%         
-% % if size(log_res, 1)==1
-%         cf = size(coef, 1);
-%         for i = 1:cf
-%             CF(i, 1) = -coef(cf + 1 - i, 1);
-%         end
-%         
-%         CF(cf + 1, 1) = 1;
-%         
-%         
-%         coef = [];
-%         coef = CF';
-%         COEF(size(COEF, 1) + 1, :) = coef;
-% % Graficos de polinomios
-% %         x = linspace(0, 5);
-% % hold on 
-% % grid on
-% % for i = 1:size(COEF, 1)
-% %      y = polyval(COEF(i, :), x);  
-% %      plot(x, y)
-% % end
-% 
-% 
-%  COEF = [];
-%    
-
-%semilogy(log_res, color)
-%semilogy(iter, log_res, 'r')
-%semilogy(log_res, 'g')
-%xlabel('Number of Restart Cycle');ylabel('|rj|/|r0|');
-%title('Residuals,  tol = 10-8');
-
-%tiempo = cputime - inicio;     %Imprime tiempo de ejecuci�n
-%figure(1)
-%subplot(1, 1, 1);
-%semilogy(log_res, color)
-
-%legend(['PD-GMRES(30;2;0.8); cycle =  ', num2str(last_cycle), ...
-%'; t =  ', num2str(tiempo), ' s.'],'Location','SouthOutside');
- 
-%title(Name_Matrix);
-%title(color);
-%xlabel('Number of Restart Cycle');ylabel('|rj|/|r0|');
-%legend(['PD-GMRES(27,alpha_{P} = ', num2str(alpha_0),',alpha_{D} = ', num2str(delta_0),'), t =  ', num2str(tiempo)],'Location','Best');
-%title(['Example 2.2 - Complementary cycles of GMRES. Nl = ', num2str(Nl),'; delta = ', num2str(dl)])
-%hold on
-%figure(2)
-%subplot(1,1,1);
-%plot(m_iteracion,color)
-%xlabel('Number of restart cycles');ylabel('m, restart parameters');
-%hold on
-
-%legend('Baker-GMRES(27,3)', 'Location','EastOutside');
-%hold on
