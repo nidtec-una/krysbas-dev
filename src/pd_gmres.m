484ad1264ff6355c63dfa1ae6af104908e5a6c95
Jhabriel Varela <jhabriel@gmail.com>
2023-11-13 14:35:44 -0300

    DEV/MAINT: Default values for `mMinMax` and `mStep` / Some code cleaning

--- src/pd_gmres.m
+++ src/pd_gmres.m
@@ -87,7 +87,6 @@ elseif nargin > 8
 end
 
 % ----> Sanity checks on matrix A
-
 % Check whether A is non-empty
 if isempty(A)
     error("Matrix A cannot be empty.")
@@ -103,7 +102,6 @@ n = rowsA;
 clear rowsA colsA;
 
 % ----> Sanity checks on vector b
-
 % Check whether b is non-empty
 if isempty(b)
     error("Vector b cannot be empty.")
@@ -122,32 +120,60 @@ end
 
 clear rowsb colsb;
 
-% ----> Default values and sanity checks for parameter m0
-
+% ----> Default values and sanity checks for parameter mInitial
 % When the restart parameter is not specified explicitly as an input
 % argument, pd_gmres() behaves identically to the unrestarted gmres()
 % with a fixed number of iterations given by min(n, 10).
 %
 % There are three possibilities when this might happen: 
 %   (1) If only two arguments are given i.e., A and b.
-%   (2) If m0 equals the dimension of A i.e., m0 = n.
-%   (3) If an empty matrix is given as restart parameter, i.e., m0 = [].
+%   (2) If mInitial equals the dimension of A i.e., mInitial = n.
+%   (3) If an empty matrix is given as mInitial, i.e., mInitial = [].
 if (nargin < 3) || isempty(mInitial) || (mInitial == n)
     restarted = false;  % use unrestarted pd_gmres()
 else
     restarted = true;  % use restarted pd_gmres()
 end
 
-if restarted
-    if mInitial <= 0
-        error("Restart parameter m0 must be a positive integer.")
-    elseif mInitial > n
-        error("Restart parameter m0 cannot be greater than n.")
+if restarted && (mInitial <= 0) || (mInitial > n)
+    error("mInitial must satisfy: 0 < mInitial <= n.")
+end
+
+% ----> Default values and sanity checks for mMinMax
+if (nargin < 4) || isempty(mMinMax)
+    mMinMax = [1; n-1];
+else
+    if ~restarted
+        warning("mMinMax was given but will not be used.")
     end
 end
 
+% TODO: Do we want to allow for mMinMax(1) = mMinMax(2)? I guess in that
+% case the PD-rule won't do anything, since m will remain constant. Right?
+if (mMinMax(1) <= 0) || (mMinMax(2) > n) || (mMinMax(2) <= mMinMax(1))
+    error("mMinMax must satisfy: 0 < mMinMax(1) < mMinMax(2) <= n.")
+end
+
+mMin = mMinMax(1);
+mMax = mMinMax(2);
+
+% ----> Default values and sanity checks for mStep
+if (nargin < 5) || isempty(mStep)
+    mStep = 1;
+else
+    if ~restarted
+        warnign("mStep was given but will not be used.")
+    end
+end
+   
+%TODO: Do we want to allow for mStep = 0? In that case, the m parameter
+% won't change. In other words, we will be doing gmres(mInitial).
+if (mStep <= 0) || (mStep >= n)
+    error("mStep must satisfy: 0 < mStep < n.")
+end
+
 % ----> Default value and sanity checks for tol
-if (nargin < 4) || isempty(tol)
+if (nargin < 6) || isempty(tol)
     tol = 1e-6;
 end
 
@@ -160,48 +186,41 @@ elseif tol >= 1
 end
 
 % ----> Default value for maxit
-if (nargin < 5) || isempty(maxit)
+% TODO: We have to have a closer look at the defualt values for maxit
+if (nargin < 7) || isempty(maxit)
     if restarted
-        % If the restarted version of pd_gmres() version must be used
-        % but maxit is not given, we take the min between n/m0 and 10.
         maxit = min(ceil(n/mInitial), 10);
     else
-        % If the unrestarted version must ...
         maxit = min(n, 10);
     end
 end
     
-% ----> Default value and sanity checks for initial guess x0
-if (nargin < 6) || isempty(xInitial) 
+% ----> Default value and sanity checks for initial guess xInitial
+if (nargin < 8) || isempty(xInitial) 
     xInitial = zeros(n, 1);
 end
 
-% Check whether x0 is a column vector
-[rowsx0, colsx0] = size(xInitial);          
-if colsx0 ~= 1
-    error("Initial guess x0 is not a column vector.")
+% Check whether xInitial is a column vector
+[rowsxInitial, colsxInitial] = size(xInitial);          
+if colsxInitial ~= 1
+    error("Initial guess xInitial is not a column vector.")
 end
 
 % Check whether x0 has the right dimension
-if rowsx0 ~= n
-    error("Initial guess x0 does not have the correct dimension.")
+if rowsxInitial ~= n
+    error("Initial guess xInitial does not have the correct dimension.")
 end
 
-clear rowsx0 colsx0;
+clear rowsxInitial colsx0;
 
-% ----> Default value for propotional parameter alphaP
-% The default value for this parameter was taken from page 217 of
-% https://doi.org/10.1016/j.cam.2018.01.009 
-if (nargin < 7) || isempty(alphaP)
-    alphaP = -3;
+% ----> Default value for alphaPD
+% The default values for these parameteres were taken from page 217 of [2]
+if (nargin < 9) || isempty(alphaPD)
+    alphaPD = [-5, 3];
 end
 
-% ----> Default value for proportional parameter alphaD
-% The default value for this parameter was taken from page 217 of
-% https://doi.org/10.1016/j.cam.2018.01.009 
-if (nargin < 8) || isempty(alphaD)
-    alphaD = 5;
-end
+alphaP = alphaPD(1);
+alphaD = alphaPD(2);
 
 % ---> Algorithm starts here 
 
@@ -212,7 +231,8 @@ end
 % residuals).
 if ~restarted
     tic();
-    [x, flag, relres, iter, resvec] = gmres(A, b, [], tol, maxit, [], [], xInitial);
+    [x, flag, relres, iter, resvec] = ...
+        gmres(A, b, [], tol, maxit, [], [], xInitial);
     resvec = [resvec(1); resvec(end)];
     time = toc();
     return
@@ -221,89 +241,97 @@ end
 % Restarted version of PD-GMRES
 
 % Algorithm setup 
-%mmin = 1;
-mmax = n-1;
-mstep =1 ;
-flag=0;
-restart=1;
-r0=b-A*xInitial;
-res(1,:)=norm(r0);
-resvec(1,:)=(norm(r0)/res(1,1));
-iter(1,:)=restart;
-mIteration(1,1)=mInitial;
+flag = 0;
+restart = 1;
+r0 = b - A * xInitial;
+res(1, :) = norm(r0);
+resvec(1, :) = (norm(r0) / res(1, 1));
+iter(1, :) = restart;
+mIteration(1, 1) = mInitial;
 
 tic();  % start measuring CPU time
 
-while flag==0
-    if iter(size(iter,1),:) ~=1
-        [miter]=pd_rule(m,mInitial,mMin,res,iter(size(iter,1),:),mstep, mmax,alphaP, alphaD);
-        m=miter(1,1);
-        mInitial=miter(1,2);
+while flag == 0
+    
+    if iter(size(iter, 1), :) ~= 1
+        [miter] = pd_rule(m, mInitial, mMin, res, ...
+            iter(size(iter, 1), :), mStep, mMax, alphaP, alphaD);
+        m=miter(1, 1);
+        mInitial=miter(1, 2);
     else
         m=mInitial;
     end
-    mIteration(iter(size(iter,1),:)+1,1)=m;
-    v=zeros(n,m+1);
-    w=zeros(n,m);
-    r=b-A*xInitial;
-    beta=norm(r);
-    v(:,1)=r/beta; 
-    h=zeros(m+1,m);
-    for j=1:m                       % Modified gram schmidt--Arnoldi
-        w(:,j)=A*v(:,j);
+    
+    mIteration(iter(size(iter, 1), :) + 1, 1) = m;
+    v = zeros(n, m + 1);
+    w = zeros(n, m);
+    r = b - A * xInitial;
+    beta = norm(r);
+    v(:, 1) = r / beta; 
+    h = zeros(m + 1, m);
+    
+    % Modified Gram Schmidt-Arnoldi
+    for j = 1:m 
+        w(:, j) = A * v(:, j);
         for i=1:j
-            h(i,j)=w(:,j)'*v(:,i);
-            w(:,j)=w(:,j)-h(i,j)*v(:,i);
+            h(i, j) = w(:, j)' * v(:, i);
+            w(:, j) = w(:, j) - h(i, j) * v(:, i);
         end
-        h(j+1,j)=norm(w(:,j));
-        if h(j+1,j)==0
-            m=j;
-            h2=zeros(m+1,m);    % Comment by JCC: VERIFY!!! (why?)
+        h(j + 1, j) = norm(w(:, j));
+        if h(j + 1, j) == 0
+            m = j;
+            h2 = zeros(m + 1, m); % Comment by JCC: VERIFY!!! (why?)
             for k=1:m
-                h2(:,k)=h(:,k);
+                h2(:, k) = h(:, k);
             end
-            h=h2;
+            h = h2;
         else        
-        v(:,j+1)=w(:,j)/h(j+1,j);
+        v(:, j + 1) = w(:, j) / h(j + 1, j);
         end
     end
-    g=zeros(m+1,1);
-    g(1,1)=beta;
-    for j=1:m                       % Plane rotations (QR decompostion)
-        P=eye(m+1);   
-        sin=h(j+1,j)/(sqrt(h(j+1,j)^2 + h(j,j)^2));
-        cos=h(j,j)/(sqrt(h(j+1,j)^2 + h(j,j)^2));
-        P(j,j)=cos;
-        P(j+1,j+1)=cos;
-        P(j,j+1)=sin;
-        P(j+1,j)=-sin;
-        h=P*h;
-        g=P*g;
+    g = zeros(m + 1, 1);
+    g(1 ,1) = beta;
+    
+    % Plane rotations (QR decompostion)
+    for j = 1:m                       
+        P = eye(m + 1);   
+        sin = h(j + 1, j) / (sqrt(h(j + 1, j)^2 + h(j, j)^2));
+        cos = h(j, j) / (sqrt(h(j + 1, j)^2 + h(j, j)^2));
+        P(j, j) = cos;
+        P(j + 1,j + 1)= cos;
+        P(j,j + 1) = sin;
+        P(j + 1,j) = -sin;
+        h = P * h;
+        g = P * g;
     end
-    R=zeros(m,m);
-    G=zeros(m,1);
-    V=zeros(n,m);
-    for k=1:m
-        G(k)=g(k);
-        V(:,k)=v(:,k);
-        for i=1:m
-            R(k,i)=h(k,i);
+    
+    R = zeros(m, m);
+    G = zeros(m, 1);
+    V = zeros(n, m);
+    
+    for k = 1:m
+        G(k) = g(k);
+        V(:, k) = v(:, k);
+        for i = 1:m
+            R(k, i) = h(k, i);
         end
     end
-    minimizer=R\G;
-    xm=xInitial+V*minimizer;
-    res(restart+1,:)=abs(g(m+1,1));
-    iter(restart+1,:)=restart+1;
-    resvec(size(resvec,1)+1,:)=abs(g(m+1,1)/res(1,1));
-    if abs(g(m+1, 1)) / res(1, 1) < tol || size(resvec, 1) == maxit % Using last component of g as residual
-        flag=1;  % solution has converged
+    
+    minimizer = R \ G;
+    xm = xInitial + V * minimizer;
+    res(restart + 1, :) = abs(g(m + 1, 1));
+    iter(restart + 1, :) = restart + 1;
+    resvec(size(resvec, 1) + 1, :) = abs(g(m + 1, 1) / res(1, 1));
+    % Use last component of g as residual
+    if abs(g(m + 1, 1)) / res(1, 1) < tol || size(resvec, 1) == maxit 
+        flag = 1;  % solution has converged
         x = xm;  % solution vector
     else
-        xInitial=xm;  %update and restart
-        restart=restart+1;
+        xInitial = xm;  % update and restart
+        restart = restart + 1;
     end
     % Compute the relative residual
-    relres = norm(b-A*xm)/norm(b);
+    relres = norm(b - A * xm) / norm(b);
 end
 
 time = toc();  % record CPU time
