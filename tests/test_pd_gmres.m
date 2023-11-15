1744f6b3f0cf8ee3460539d755d04a4d3b98efce
Jhabriel Varela <jhabriel@gmail.com>
2023-11-15 15:50:15 -0300

    TST: Good progress with tests for sanity checks

--- tests/test_pd_gmres.m
+++ tests/test_pd_gmres.m
@@ -13,7 +13,7 @@ function test_suite = test_pd_gmres %#ok<*STOUT>
 end
 
 
-% ----> Test for sanity checks
+% ----> Test sanity checks and default values
 
 function test_number_of_input_arguments() 
 % Test if error is raised when passing incorrect number of inputs.  
@@ -22,7 +22,7 @@ function test_number_of_input_arguments()
         pd_gmres(ones(2, 2));
     catch ME
         msg = "Too few input parameters. Expected at least A and b.";
-        assert(ME.message == msg)
+        assert(matches(ME.message, msg))
     end
 
     % Error should be raised since 10 parameters are given
@@ -30,6 +30,78 @@ function test_number_of_input_arguments()
         pd_gmres([], [], [], [], [], [], [], [], [], []);
     catch ME
         msg = "Too many input parameters.";
-        assert(ME.message == msg)
+        assert(matches(ME.message, msg))
     end     
 end
+
+function test_empty_matrix_A()
+% Test if error is raised when an empty matrix A is given.
+    try
+        pd_gmres([], 1);
+    catch ME
+        msg = 'Matrix A cannot be empty.';
+        assert(matches(ME.message, msg))
+    end
+end
+
+function test_non_square_matrix_A()
+% Test if error is raised when matrix A is not squared
+    try
+        pd_gmres([1; 1], [1; 1]);
+    catch ME
+        msg = "Matrix A must be square.";
+        assert(matches(ME.message, msg))
+    end
+end
+
+function test_empty_vector_b()
+% Test if error is raised when vector b is empty
+    try
+        pd_gmres(1, []);
+    catch ME
+        msg = "Vector b cannot be empty.";
+    end
+end
+
+function test_vector_b_not_column_vector()
+% Test if error is raised when vector b is not a column vector
+    try
+        pd_gmres(ones(2), [1, 1]);
+    catch ME
+        msg = "Vector b must be a column vector.";
+    end
+end
+
+function test_size_compatibility_between_A_and_b()
+% Test if error is raised when the dimensionality of A and b differs
+    try
+        pd_gmres(ones(3), [1; 1]);
+    catch ME
+        msg = "Dimension mismatch between matrix A and vector b.";
+    end
+end
+
+function test_mInitial_not_given_empty_or_equal_to_n()
+% Test whether the unrestarted version of pd_gmres() is used if mInitial is
+% empty, not given, or equal to the dimension of the linear system n
+
+    % Here, we actually solve a linear system and check whether
+    % 'restarted' is false or not
+
+    A = eye(3);
+    b = ones(3, 1);
+    
+    % Only provide A and b
+    [~, ~, ~, ~, ~, restarted, ~] = pd_gmres(A, b);
+    assert(restarted == false);
+
+    % Pass mInitial empty
+    [~, ~, ~, ~, ~, restarted, ~] = pd_gmres(A, b, []);
+    assert(restarted == false);
+
+    % Pass mInitial = 3
+    [~, ~, ~, ~, ~, restarted, ~] = pd_gmres(A, b, 3);
+    assert(restarted == false);
+
+end
+
