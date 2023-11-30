4a18e2a9fa3c30e6edb2b9c2cbccf0eeed90102f
Jhabriel Varela <jhabriel@gmail.com>
2023-11-30 14:12:45 -0300

    DOC/MAINT: Better comments / Small improvements on code

--- src/pd_gmres.m
+++ src/pd_gmres.m
@@ -338,20 +338,25 @@ function [x, flag, relresvec, mvec, time] = ...
         gm = g(1:m);
         minimizer = Rm \ gm;
         xm = xInitial + V * minimizer;       
+        
+        % Update residual norm, iterations, and relative residual vector
         res(restart + 1, :) = abs(g(m + 1, 1));
         iter(restart + 1, :) = restart + 1;
-        relresvec(size(relresvec, 1) + 1, :) = abs(g(m + 1, 1) / res(1, 1));
+        relresvec(size(relresvec, 1) + 1, :) = res(restart + 1, :) / res(1, 1);
         
-        % Use last component of g as residual
-        if abs(g(m + 1, 1)) / res(1, 1) < tol || size(relresvec, 1) == maxit
-            flag = 1;  % solution has converged
-            x = xm;  % solution vector
+        % Check convergence
+        if relresvec(end) < tol || size(relresvec, 1) == maxit
+            % We reached convergence.
+            flag = 1;
+            x = xm;
         else
-            xInitial = xm;  % update and restart
+            % We have not reached convergence. Update and restart.
+            xInitial = xm;
             restart = restart + 1;
         end
+    
     end
 
-    time = toc();  % record CPU time
+    time = toc(); 
 
 end
