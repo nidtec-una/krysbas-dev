90c0fd15976f983cd1c7c238a1b7d768532593e6
Jhabriel Varela <jhabriel@gmail.com>
2023-11-30 15:55:37 -0300

    WIP: Print current data folder

--- tests/test_pd_gmres.m
+++ tests/test_pd_gmres.m
@@ -340,6 +340,7 @@ function test_embree_three_by_three_toy_example()
     % Load A and b
     rootFolder = fileparts(pwd); % go to the root folder
     dataFolder = fullfile(rootFolder, 'data'); % enter the data folder
+    disp(dataFolder);
     load 'embree3.mat' Problem;
     A = Problem.A;
     b = Problem.b;
