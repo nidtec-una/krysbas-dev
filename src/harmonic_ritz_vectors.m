98acba6bd55bd420b757c7c0fed5493ea95792aa
Gustavo Espínola Mena <gustavoespinola1989@gmail.com>
2024-09-11 11:02:06 -0400

    DOC: editing docstring

--- src/harmonic_ritz_vectors.m
+++ src/harmonic_ritz_vectors.m
@@ -1,42 +1,41 @@
-function dy = harmonic_ritz_vectors(Fold, G, k, V, tol)
+function dy = harmonic_ritz_vectors(F, G, k, V, tol)
     % Harmonic Ritz Vectors function
     %
     %   This function is a modified implementation of the Rayleigh-Ritz
     %   method that finds good approximations to the smallest eigenvalues
-    %   and its associated eigenvectors, and aggregates these ones
-    %   to the succesive search subspaces. Please refer to [1] for
-    %   more information about this technique.
+    %   and its associated eigenvectors, and appends these ones to the next
+    %   search subspaces. Please refer to [1] for more information about
+    %   this technique.
     %
     %   Signature:
     %   ----------
     %
-    %   dy = harmonic_ritz_vectors(Fold, G, k, V, tol)
+    %   dy = harmonic_ritz_vectors(F, G, k, V, tol)
     %
     %
     %   Input Parameters:
     %   -----------------
     %
-    %   Fold:       s-by-s matrix
+    %   F:          s-by-s maxtrix
+    %               Matrix F from the generalized eigenvalue problem, as
+    %               employed in step 5, p. 1161 of [1].
     %
     %   G:          s-by-1 vector
+    %               Matrix G from the generalized eigenvalue problem, as
+    %               employed in step 5, p. 1161 of [1].
     %
     %   k:          int
-    %               Number of eigenvectors corresponding to
-    %               a few of the smallest eigenvalues in magnitude
-    %               for each outer iteration. Default is 3, but values
-    %               between 1 and 5 are mostly used.
-    %               According to [1], "even just a few eigenvectors can make
-    %               a big difference if the matrix has both small and large
-    %               eigenvalues".
-    %               If m < n AND k == 0, built-in gmres(m) will be used.
+    %               Number of eigenvectors corresponding to a few of the
+    %               smallest eigenvalues in magnitude. According to [1],
+    %               "even just a few eigenvectors can make a big difference
+    %               if the matrix has both small and large eigenvalues".
     %
     %   V:          n-by-s matrix
     %               A matrix from the modified Gram-Schmidt Arnoldi method,
     %               whose column vectors span the search subspace.
     %
-    %   tol:        float, optional
-    %               Convergence tolerance
-    %               Default is 1e-6.
+    %   tol:        float
+    %               Convergence tolerance for the built-in 'eigs' algorithm.
     %
     %
     %   Output parameters:
@@ -44,15 +43,14 @@ function dy = harmonic_ritz_vectors(Fold, G, k, V, tol)
     %
     %   dy:         n-by-k matrix
     %               Matrix whose column vectors are the approximate
-    %               eigenvectors, called "harmonic Ritz vectors",
-    %               of the LHS matrix A.
+    %               eigenvectors of the matrix A.
     %
     %   References:
     %   -----------
     %
-    %   [1] Morgan, R. B. (1995). A restarted GMRES method augmented
-    %   with eigenvectors. SIAM Journal on Matrix Analysis and
-    %   Applications, 16(4), 1154-1171.
+    %   [1] Morgan, R. B. (1995). A restarted GMRES method augmented with
+    %   eigenvectors. SIAM Journal on Matrix Analysis and Applications,
+    %   16(4), 1154-1171.
     %
     %   Copyright:
     %   ----------
@@ -75,13 +73,13 @@ function dy = harmonic_ritz_vectors(Fold, G, k, V, tol)
     %   with this file.  If not, see <http://www.gnu.org/licenses/>.
     %
     opts.tol = tol;
-    s = size(Fold, 1);
+    s = size(F, 1);
     opts.v0 = ones(s, 1);
     E = zeros(s, k);
     D = zeros(k, 1);
 
     % Compute the approximate eigenvectors
-    [E2, D2] = eigs(Fold, G, k, 'LM', opts);
+    [E2, D2] = eigs(F, G, k, 'LM', opts);
     for p = 1:k
         D(p, 1) = abs(D2(p, p));
     end
@@ -89,12 +87,13 @@ function dy = harmonic_ritz_vectors(Fold, G, k, V, tol)
     for q = 1:k
         E(:, q) = E2(:, I(q, 1));
     end
-    dy0 = V * E; % Substitutes yi = Q * gi
+    dy0 = V * E; % Implements yi = Q * gi, step 5, p. 1161 of [1]
 
     dy = [];
 
-    % If dy0 has complex components, we separate each eigenvector
-    % into real and complex parts and treat as two distinct vectors
+    % If dy0 has complex components, its complex conjugate is also
+    % an approximate eigenvector, hence we separate each eigenvector
+    % into its real and complex parts and treat as two distinct vectors
     if isreal(dy0) == 0
         ij = 1;
         jj = 0;
